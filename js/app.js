8f0a5cd4334b09b1d3f6186e90d86c6665572d30
Timmyturner1886 <anzigalet@gmail.com>
2023-07-16 22:40:22 +0300

    added calculation function

--- js/app.js
+++ js/app.js
@@ -2,9 +2,32 @@ const screenDisplay = document.querySelector('.screen')
 const button = document.querySelectorAll('button') 
 console.log(button)
 
+let calculation = []
+
+
 function calculate(button) {
+   //collects the content of the button 
     const value = button.textContent
     
+
+    if (value === "CLEAR") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    }
+    else if (value === "=") {
+        screenDisplay.textContent = eval(calculation.join(''))
+        calculation = []
+    }
+    else {
+        calculation.push(value)
+    
+    console.log(calculation)
+
+    screenDisplay.textContent = calculation.join('')
+
 }
+    
+}
+//call back to calculate when the button is clicked
 
 button.forEach(button => button.addEventListener('click',() => calculate(button)));
